f399809259b37f56eb468b50e354456a5861930f
Inna Yasynchuk <i_bybyk@ukr.net>
2023-03-26 21:08:50 +0300

    implemented form validation

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
